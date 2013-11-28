1cb802763cfcd757bbf9595f16f24848203b8672
Dmitry Savin <savin.dmitry@gmail.com>
2013-11-28 06:11:24 +0200

    Bag form for adding items

---
+++ apps/frontend/modules/default/templates/_bagForm.php
@@ -0,0 +1,14 @@
+<?php echo form_tag('@homepage') ?>
+<table id="bag_form">
+    <tfoot>
+    <tr>
+        <td colspan="2">
+            <input type="submit" value="Упакуйся!" />
+        </td>
+    </tr>
+    </tfoot>
+    <tbody>
+    <?php echo $form ?>
+    </tbody>
+</table>
+</form>
