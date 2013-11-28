25c04a71815a4dfd34ac10e76f3a08426b39a02f
Dmitry Savin <savin.dmitry@gmail.com>
2013-11-28 06:12:11 +0200

    Bag form for adding items

--- apps/frontend/modules/default/templates/_addItemsBagForm.php
+++ apps/frontend/modules/default/templates/_addItemsBagForm.php
@@ -1,9 +1,9 @@
-<?php echo form_tag('@homepage') ?>
-<table id="bag_form">
+<?php echo form_tag('@bag_items?hash=' . $hash) ?>
+<table id="add_item_bag_form">
     <tfoot>
     <tr>
         <td colspan="2">
-            <input type="submit" value="Упакуйся!" />
+            <input type="submit" value="Добавить!" />
         </td>
     </tr>
     </tfoot>
