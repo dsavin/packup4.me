289b9ba5971157f140c87283f39b66f8e9d14b49
Dmytro Zadorozhniy <dmitriyzadorozniy@gmail.com>
2014-05-19 14:58:24 +0300

    added angular and basic bootstrap theme

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -1,30 +1,55 @@
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap 101 Template</title>
-
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<!--[if lt IE 7]>      <html lang="en" ng-app="packupApp" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" ng-app="packupApp" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" ng-app="packupApp" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" ng-app="packupApp" class="no-js"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>My AngularJS App</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include_http_metas() ?>
     <?php include_metas() ?>
     <?php include_title() ?>
     <?php include_stylesheets() ?>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="page-header">
+          <h1>Packup4.me - <small>helping you getting stuff packed</small></h1>
+          <div class="navbar navbar-inverse">
+            <div class="navbar-header">
+              <a href="#/main" class="navbar-brand">Home</a>
+            </div>
+            <div class="navbar-collapse collapse navbar-responsive-collapse">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#/contact">Contact</a></li>
+                <li><a href="#/something">Something else here</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
+  <!--[if lt IE 7]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
 
-  </head>
-  <body>
-    <?php echo $sf_content ?>
+  <div ng-view></div>
 
-    <?php include_javascripts() ?>
-  </body>
+  <!-- <?php echo $sf_content ?> -->
+  
+
+  <!-- In production use:
+  <script src="//ajax.googleapis.com/ajax/libs/angularjs/x.x.x/angular.min.js"></script>
+  -->
+    
+
+  </div>
+  <?php include_javascripts() ?>
+</body>
 </html>
